e5a2bc736e540d51b1b5e0da4468b0eccea4afba
LudovicAns <ludovicanselin@gmail.com>
2020-11-24 16:12:32 +0100

    Updated Makefile and linft.h. Added ft_putendl_fd.c

--- Makefile
+++ Makefile
@@ -30,6 +30,7 @@ SRC = ft_atoi.c \
 		ft_memmove.c \
 		ft_memset.c \
 		ft_putchar_fd.c \
+		ft_putendl_fd.c \
 		ft_putstr_fd.c \
 		ft_split.c \
 		ft_strchr.c \
--- libft.h
+++ libft.h
@@ -48,5 +48,6 @@ char	*ft_strcpy(char *dest, char *src);
 char    *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_putchar_fd(char c, int fd);
 void    ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 
 #endif
