c5d312af1948939ad9c2cde8950525f845cce9b1
LudovicAns <ludovicanselin@gmail.com>
2020-11-25 17:46:25 +0100

    Removed forbidden import.

--- ft_split.c
+++ ft_split.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 static void fill_tab(char **tab, char const *s, char c)
 {
