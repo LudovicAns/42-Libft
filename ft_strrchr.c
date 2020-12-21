26a6bd0ef2509722102abc99c6dfb99ced4eeb16
Ludovic Anselin <lanselin@z1r1p4.42lyon.fr>
2020-12-21 14:19:58 +0100

    Fixed norme on ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: lanselin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/20 16:36:11 by lanselin          #+#    #+#             */
-/*   Updated: 2020/12/20 16:36:28 by lanselin         ###   ########lyon.fr   */
+/*   Updated: 2020/12/21 14:19:20 by lanselin         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int i;
-	char ccast;
+	int		i;
+	char	ccast;
 
 	i = 0;
 	while (s[i])
