6cd5dbc8452da803ed07f050aaabeade75dc273b
Ludovic Anselin <lanselin@z1r1p4.42lyon.fr>
2020-12-21 14:33:01 +0100

    Fixed ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: lanselin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/20 16:34:05 by lanselin          #+#    #+#             */
-/*   Updated: 2020/12/20 16:34:19 by lanselin         ###   ########lyon.fr   */
+/*   Updated: 2020/12/21 14:29:41 by lanselin         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n)
+	while ((s1[i] || s2[i]) && (i < n))
 	{
 		if (s1[i] != s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
