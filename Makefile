63175a47c01dddf025cde77aa34a9aa860d8934d
LudovicAns <ludovicanselin@gmail.com>
2021-01-08 01:21:25 +0100

    Fixed memmove. (Forbidden VLA)

--- Makefile
+++ Makefile
@@ -89,5 +89,5 @@ fclean: clean
 
 re: fclean all
 
-.PHONY: all, clean, fclean, re
+.PHONY: all clean fclean re
 
--- ft_memmove.c
+++ ft_memmove.c
@@ -14,9 +14,20 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char	temp[n];
+	unsigned char		*ptr_dest;
+	const unsigned char	*ptr_src;
 
-	ft_memcpy(temp, src, n);
-	ft_memcpy(dest, temp, n);
-	return (dest);
+	ptr_dest = dest;
+	ptr_src = src;
+
+	if (ptr_src < ptr_dest)
+		while (n--)
+			ptr_dest[n] = ptr_src[n];
+	else
+	{
+		while (n--)
+			*ptr_dest++ = *ptr_src++;
+		
+	}
+	return(dest);
 }
